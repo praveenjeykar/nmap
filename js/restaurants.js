122730e92da8972ced4622379392f4f1bca0ddcb
praveenjeykar <38877220+praveenjeykar@users.noreply.github.com>
2018-06-19 10:05:59 +0530

    Add files via upload

---
+++ js/restaurants.js
@@ -0,0 +1,68 @@
+var myLocations = [
+    {
+        title: 'Yati',
+        lat: 16.964113,
+        lng: 82.2273401,
+        type: 'Restaurant'
+    },
+    {
+        title: 'The Costa Gril',
+        lat: 16.973391,
+        lng: 82.2349693,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Haveli',
+        lat: 16.9689,
+        lng: 82.2370,
+        type:  'Resturant'
+    },
+    {
+        title: 'Hungry Birds',
+        lat: 16.9702,
+        lng: 82.2419,
+        type:  'Restaurant'
+    },
+    {
+        title: 'Eat More',
+        lat: 16.971929,
+        lng: 82.2434917,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Bakers Inn',
+        lat: 16.9691507,
+        lng: 82.2347829,
+        type: 'Restaurant'
+    },
+    {
+        title: 'E2',
+        lat: 16.971303,
+        lng: 82.2018052,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Unnat',
+        lat: 16.971303,
+        lng: 82.2018052,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Seasons5',
+        lat: 16.9695089,
+        lng: 82.2179027,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Temptations',
+        lat: 16.9701141,
+        lng: 82.2377974,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Subway',
+        lat: 16.9575588,
+        lng: 82.233476,
+        type: 'Restaurant'
+    }
+]
